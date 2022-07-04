72126bbb4b01d7f56bbdc570ce0d4945d266c9d6
Xurshidbek-st <xurshidurishboyev1998@gmail.com>
2022-07-04 18:06:43 +0500

    second commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const favouriteColor = [];
+favouriteColor[0] = prompt("What is favourite color #1");
+favouriteColor[1] = prompt("What is favourite color #2");
+favouriteColor[2] = prompt("What is favourite color #3");
+favouriteColor[3] = prompt("What is favourite color #4");
+console.log(favouriteColor);
